9fc7660380a119e2b864036989f4600b03d4e16c
Danila Denisov <daniladenisov1is@gmail.com>
2025-06-11 13:20:40 +0300

    Done with domain models

---
+++ TaskManagement/src/TaskManagement.Domain/Tasks/TaskDelivery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Tasks
+{
+    public class TaskDelivery
+    {
+        public Guid Id { get; set; }
+
+        public required Guid TaskItemId { get; set; }
+
+        public required string Comment { get; set; }
+
+        public List<string> Attachments { get; set; } = [];
+
+        public required Guid TeamMemberId { get; set; }
+
+        public DateTime SentDate { get; set; }
+    }
+}
---
+++ TaskManagement/src/TaskManagement.Domain/Teams/Team.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Teams
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public string? PicturePath { get; set; }
+
+        public required string InviteUrl { get; set; } 
+
+        public required Guid UserId { get; set; }
+
+        public List<TeamMember> TeamMembers { get; set; } = [];
+    }
+}
