f68e352e627e8408a0f191a9e278b93b2f395c31
Danila Denisov <daniladenisov1is@gmail.com>
2025-06-11 16:25:31 +0300

    Create base http methods for users

---
+++ TaskManagement/src/TaskManagement.Contracts/Users/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Contracts.Users
+{
+    public record LoginRequest(string Mail, string password);
+}
---
+++ TaskManagement/src/TaskManagement.Web/Program.cs
@@ -0,0 +1,29 @@
+
+using Microsoft.AspNetCore.Builder;
+
+namespace TaskManagement.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "TaskManagement"));
+            }
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
